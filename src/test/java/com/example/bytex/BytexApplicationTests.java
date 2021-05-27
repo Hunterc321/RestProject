f262c650187d4f861b9fb81136d34f317f5d3fcf
popacosmin <popa.cosmin@ac.tuiasi.ro>
2021-05-27 16:05:26 +0300

    first commit

---
+++ src/test/java/com/example/bytex/BytexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bytex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BytexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
